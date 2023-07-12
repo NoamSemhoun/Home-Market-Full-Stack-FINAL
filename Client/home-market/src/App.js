cb80f2236df2adbe0420fefc965f6ab49351b2a4
NoamSemhoun <noam@semhoun.eu>
2023-07-12 23:47:20 +0300

    INIT

---
+++ Client/home-market/src/App.js
@@ -0,0 +1,30 @@
+import logo from './Images/homeMarketLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          THIS IS IN CONSTRUCTION ! <br></br>
+          Home market in developpement 
+          <br></br><br></br>
+          see you ...
+        </p>
+
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

cc45bf41a5e16a4e81f6bf8a8a8c76d8759e589f
NoamSemhoun <noam@semhoun.eu>
2023-07-20 21:32:46 +0300

    ok FORM

--- Client/home-market/src/Selling.jsx
+++ Client/home-market/src/Selling.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 import {
     MDBRow,
     MDBAccordion, MDBAccordionItem, MDBIcon,
     MDBCol,
     MDBInput,
+    MDBInputGroup,
     MDBCheckbox,
     MDBBtn
   } from 'mdb-react-ui-kit';
@@ -30,38 +32,118 @@ function Selling() {
                        
                     <form>
                         <MDBRow className='mb-4'>
-                            <MDBCol>
-                            <MDBInput id='form6Example1' label='Name' />
+
+                            <MDBCol size="4" >
+                            <MDBInput size="lg" 
+                             defaultValue='Table IKEA name' 
+                            //  style= 'color = gray'
+                              id='form6Example1' label='Tittle' />
                             </MDBCol>
-                            <MDBCol>
-                            <MDBInput id='form6Example2' label='Brand name' />
+
+                    
+
+                            <MDBCol size="4" >
+                            <Form.Select label='Category ' size="lg"  >
+                                  <option>Select Category </option> 
+                                  {/* mettre par default  */}
+                                <option>Sofa </option>
+                                <option>Bed </option>
+                                <option>Table </option>
+
+                            </Form.Select>
+                            <br />
+                            </MDBCol>
+                            
+
+                            <MDBCol size="4">
+                                <MDBInputGroup size="lg"   textAfter={['.00','$' ]}>
+                                    <input className='form-control' type='number' />
+                                </MDBInputGroup>
+                                Price
                             </MDBCol>
                             
                         </MDBRow>
 
-                        <MDBInput wrapperClass='mb-4' id='form6Example3' label='Company name' />
-                        <MDBInput wrapperClass='mb-4' id='form6Example4' label='Description' />
-                        <MDBInput wrapperClass='mb-4' type='email' id='form6Example5' label='Price' />
+
+                        <MDBRow className='mb-4'>
+
+                            <MDBCol size="4" >
+                            <MDBInput size="lg" 
+                            defaultValue='IKEA ' 
+                            //  style= 'color = gray'
+                            id='form6Example1' label='Brand Name' />
+                            </MDBCol>
+
+
+
+                            <MDBCol size="4" >
+                            <Form.Select label='Category ' size="lg"  >
+                                <option>Select State Condition </option> 
+                                {/* mettre par default  */}
+                                <option>New </option>
+                                <option>Excellent / Like New  </option>
+                                <option>Good </option>
+                                <option>Fair / Used  </option> 
+                                <option>Refurbished  </option>
+                                <option>Needs Repair </option>
+                                <option>For Parts </option>
+
+
+                            </Form.Select>
+                            <br />
+                            </MDBCol>
+
+                            <MDBCol size="4" >
+
+                                <MDBCheckbox
+                                wrapperClass='d-flex justify-content-center mb-4'
+                                id='form6Example8'
+                                label=' Negociable '
+                                defaultChecked
+                               />
+
+                                <MDBCheckbox
+                                    wrapperClass='d-flex justify-content-center mb-4'
+                                    id='form6Example8'
+                                    label=' Delivery possible '
+                                    defaultChecked
+                                /> 
+                          
+                            </MDBCol>
+
+                            </MDBRow>
+
+
+                        <MDBInput wrapperClass='mb-4' id='form6Example3' label='URL Brand ' />
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Description area</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>                        
+                        
 
                         <MDBInput wrapperClass='mb-4' textarea id='form6Example7' rows={4} label='Additional information' />
 
-                        <MDBCheckbox
-                            wrapperClass='d-flex justify-content-center mb-4'
-                            id='form6Example8'
-                            label=' Negociable '
-                            defaultChecked
-                        />
+                        
 
                     </form>
 
                     </MDBAccordionItem>
                     <MDBAccordionItem collapseId={2} headerTitle={<><MDBIcon fas icon="question-circle" /> &nbsp; #2 Photos article </>}>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        
+                        Including photos in a furniture listing is essential for attracting potential buyers, 
+                        building trust, and providing visual information. 
+                        It enhances the listing's appeal and helps buyers make informed decisions.
+                        <MDBInputGroup
+                            className='mb-3 mt-5'
+                            textBefore='Upload'
+                            textTag='label'
+                            textProps={{ htmlFor: 'inputGroupFile01' }}
+                        >
+                            <input className='form-control' type='file' id='inputGroupFile01' />
+                        </MDBInputGroup>
+                    
+                    
                     </MDBAccordionItem>
                     <MDBAccordionItem collapseId={3} headerTitle={<><MDBIcon fas icon="question-circle" /> &nbsp; #3  Personnal Information </>}>
                         
